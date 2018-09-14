e592044b4c76b063a5603bc9d382aa0d04615345
Julia Dibo <judibo@Julias-MacBook-Air.local>
2018-09-14 12:42:50 -0700

    Finished laying out UI

--- js/bomb.js
+++ js/bomb.js
@@ -4,3 +4,18 @@ document.addEventListener("DOMContentLoaded", function() {
   console.log("DOM loaded");
 });
 
+
+/*----- constants -----*/
+
+// how long the bomb timer should show at the beginning.
+// how long the timer currently has left.
+// whether or not the game is over
+// which wires are cut and uncut
+// which wires are the correct wires to cut
+
+
+
+/*----- app's state (variables) -----*/
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+/*----- functions -----*/
